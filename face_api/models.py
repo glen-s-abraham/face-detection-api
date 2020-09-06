5f680c8deecc84493ea400a5340bb5eb6b3f1874
glen-s-abraham <glenprojects666@gmail.com>
2020-09-06 08:43:02 +0530

    face api finished

--- face_api/models.py
+++ face_api/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class KnowledgeDatabase(models.Model):
 	"""Update and store images to comapre uploaded ones"""
 	name=models.CharField(max_length=50)
-	image=models.FileField(upload_to='media/known_images')
+	image=models.FileField(upload_to='/known_images')
 	def __str__(self):
 		return self.name
 
 class ImageUploads(models.Model):
 	"""To hold teh uploaded images"""
-	image=models.FileField(upload_to='media/uploaded_images')
+	image=models.FileField(upload_to='/uploaded_images')
 	def __str__(self):
 		return self.image.name
